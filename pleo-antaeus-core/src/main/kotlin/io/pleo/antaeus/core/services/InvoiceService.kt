6acab66aefd095706f1143eb02764d8ff21bed4a
Damian Potyralski <potyralski.damian@gmail.com>
2021-08-04 14:42:23 +0200

    InvoiceService extended with invoice status change possibility

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/InvoiceService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/InvoiceService.kt
@@ -7,6 +7,7 @@ package io.pleo.antaeus.core.services
 import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
 import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 
 class InvoiceService(private val dal: AntaeusDal) {
     fun fetchAll(): List<Invoice> {
@@ -16,4 +17,17 @@ class InvoiceService(private val dal: AntaeusDal) {
     fun fetch(id: Int): Invoice {
         return dal.fetchInvoice(id) ?: throw InvoiceNotFoundException(id)
     }
+
+    fun fetchPending(): List<Invoice> {
+        return dal.fetchInvoicesByInvoiceStatus(InvoiceStatus.PENDING)
+    }
+
+    fun markInvoiceAsPaid(invoice: Invoice) {
+        return dal.changeInvoiceStatus(invoice.id, InvoiceStatus.PAID)
+    }
+
+    fun markInvoiceAsUnpaid(invoice: Invoice) {
+        return dal.changeInvoiceStatus(invoice.id, InvoiceStatus.UNPAID)
+    }
+
 }
--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
@@ -2,14 +2,21 @@ package io.pleo.antaeus.core.services
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import io.pleo.antaeus.models.Money
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
 
 class InvoiceServiceTest {
-    private val dal = mockk<AntaeusDal> {
+    private val dal = mockk<AntaeusDal>(relaxUnitFun = true) {
         every { fetchInvoice(404) } returns null
+        every { fetchInvoicesByInvoiceStatus(InvoiceStatus.PENDING) } returns listOf(createInvoice())
     }
 
     private val invoiceService = InvoiceService(dal = dal)
@@ -20,4 +27,46 @@ class InvoiceServiceTest {
             invoiceService.fetch(404)
         }
     }
+
+    @Test
+    fun `should call dal for only pending invoices`() {
+        //when
+        invoiceService.fetchPending()
+
+        //then
+        verify {
+            dal.fetchInvoicesByInvoiceStatus(InvoiceStatus.PENDING)
+        }
+    }
+
+    @Test
+    fun `should mark invoice as paid`() {
+        //given
+        val testInvoice = createInvoice()
+
+        //when
+        invoiceService.markInvoiceAsPaid(testInvoice)
+
+        //then
+        verify {
+            dal.changeInvoiceStatus(1, InvoiceStatus.PAID)
+        }
+    }
+
+    @Test
+    fun `should mark invoice as unpaid`() {
+        //given
+        val testInvoice = createInvoice()
+
+        //when
+        invoiceService.markInvoiceAsUnpaid(testInvoice)
+
+        //then
+        verify {
+            dal.changeInvoiceStatus(1, InvoiceStatus.UNPAID)
+        }
+    }
+
+    private fun createInvoice() = Invoice(1, 1, Money(BigDecimal("12"), Currency.EUR), status = InvoiceStatus.PENDING)
+
 }
